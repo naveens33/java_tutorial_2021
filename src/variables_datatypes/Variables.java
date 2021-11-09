6181486391578c998437b67a04deb8e4324a8d38
naveens33 <naveensagayaselvaraj@gmail.com>
2021-11-09 19:12:02 +0530

    Variables, DataTypes, TypeCasting, ConditionalStatements

---
+++ src/variables_datatypes/Variables.java
@@ -0,0 +1,18 @@
+package variables_datatypes;
+
+public class Variables {
+    public static void main(String[] args){
+        /*
+        1. Starts with letters, underscore
+        2. Contain letters, numbers, underscore and $
+        3. Apart from $ and _ no other special characters are allowed
+        4. Variable name cannot be reserved words/identifiers
+         */
+
+        int firstNum = 5;
+        int secondNum = 6;
+        int ans = firstNum+secondNum;
+        System.out.println(ans);
+
+    }
+}
