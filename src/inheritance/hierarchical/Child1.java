8eaed3e2fb6bc18e3c51d2b51a9e35d71626d87a
Naveen S <82922481+naveens-tactai@users.noreply.github.com>
2023-06-15 13:08:49 +0530

    New topics are updated

--- src/inheritance/hierarchical/Child1.java
+++ src/inheritance/hierarchical/Child1.java
@@ -1,7 +1,7 @@
 package inheritance.hierarchical;
 
 public class Child1 extends Parent{
-    public static void main(String args[]){
+    public static void main(String[] args){
         Child1 obj = new Child1();
         obj.display();
     }
--- src/inheritance/multilevel/Child.java
+++ src/inheritance/multilevel/Child.java
@@ -1,7 +1,7 @@
 package inheritance.multilevel;
 
 public class Child extends Parent{
-    public static void main(String args[]){
+    public static void main(String[] args){
         Child obj = new Child();
         obj.display1();
         obj.display2();
